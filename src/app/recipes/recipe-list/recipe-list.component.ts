3e55450c3156f5000ecb106156129521cea99e4a
TomasGiannini <tomasjgiannini@gmail.com>
2021-04-14 12:47:41 -0400

    Added to Recipe List component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  // creating an array of Recipe array objects. Import line at the top of this file.
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_960_720.jpg')
+  ];
 
   constructor() { }
 
