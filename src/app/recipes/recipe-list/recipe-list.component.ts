bf61fca0c5a6125a792293fbbf315b08f9e512d7
TomasGiannini <tomasjgiannini@gmail.com>
2021-04-14 13:20:17 -0400

    Added more buttons and features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   // creating an array of Recipe array objects. Import line at the top of this file.
   recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_960_720.jpg'),
     new Recipe('A test recipe', 'This is a test', 'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_960_720.jpg')
   ];
 
